00631df81e1ee67b74d1c6a89f239909ae2b3237
Shahheerr <159336048+Shahheerr@users.noreply.github.com>
2024-10-19 10:08:04 +0500

    Update App.tsx

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -61,7 +61,7 @@ export default function App() {
       </section>
 
       {/* Latest Projects Section */}
-      <section>
+      <section className='mr-5 ml-5'>
         <h2 className="text-2xl font-semibold text-slate-800 mb-6">Latest Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project) => (
